6d47c92afd5563a3f9a528bb1eab539b72612ae5
Boris <boris.bortnitskii@yandex.by>
2022-08-09 00:49:20 +0400

    fix

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-module.exports = function toReadable(number) {
+function toReadable(number) {
     let number1 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     let number2 = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'hundred'];
     let string = '';
@@ -6,27 +6,15 @@ module.exports = function toReadable(number) {
 
     switch (number.toString().split('').length) {
         case 3:
-            string += number1[Math.trunc(number / 100)) + ' hundred';
-            if (number % 100 > 19) {
-                string += (number2[Math.trunc(number / 10) - Math.trunc(number / 100)]) + '' + number1[number % 10];
-            }
-            else {
-                string += (number2[Math.trunc(number / 10) - Math.trunc(number / 100)]) + '' + number1[number % 100];
-            }
+            string += number1[Math.trunc(number / 100)] + ' hundred ';
         case 2:
             if (number % 100 > 19) {
-                string += (number2[Math.trunc(number / 10) - Math.trunc(number / 100)]) + '' + number1[number % 10];
+                string += (number2[(Math.trunc(number / 10)) % 10 - 2]) + ' ';
             }
             else {
-                string += (number2[Math.trunc(number / 10) - Math.trunc(number / 100)]) + '' + number1[number % 100];
+                return string + number1[number % 100];
             }
         case 1:
-            string += number1[number]
+            return string + number1[number % 10];
     }
-    return string;
 }
-
-console.log(toReadable(997))
-console.log(toReadable(1))
-toReadable(1); // Will return 'one'
-toReadable(997); //will return 'nine hundred ninety seven'
